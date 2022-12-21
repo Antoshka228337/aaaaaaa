2b31cd8c039ad7326ab4b1fd518faf94dabf1671
oleg22 <oleg.rodin.2022@gmail.com>
2022-12-21 09:20:50 +0300

    first commit

---
+++ 3-2.py
@@ -0,0 +1,4 @@
+names = ['sasha', 'masha', 'petya'] # Вводим переменную с именами
+print('Hello, ' + names[0] + '!') # Приветствуем с помощью print гостя
+print('Hello, ' + names[1] + '!') # Приветствуем с помощью print гостя
+print('Hello, ' + names[2] + '!') # Приветствуем с помощью print гостя
